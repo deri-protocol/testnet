ab0c340475daa0d73ece3566de42d9c807e2e4d5
wenchao.huang <wenchao.huang@dfactory.tech>
2021-04-19 22:55:40 +0800

    pull

--- faucetstatic/chain/Faucet.js
+++ faucetstatic/chain/Faucet.js
@@ -14,9 +14,6 @@ $(function () {
                     "***" +
                     account.slice(account.length - 4, account.length);
                 $('#connect-wallet').hide();
-                if (ethereum.networkVersion != '3') {
-                    $('#wrong').text('(Wrong Network!)')
-                }
                 $(".id").text(account);
                 $("#address").text(address)
             } else {
@@ -31,13 +28,12 @@ $(function () {
         await contract._bindEvent()
     }
     $('#obtainU').on('click', addU);
-    $('#obtainD').on('click', addD);
     $('#obtainB').on('click', addB);
-    $('#obtainBn').on('click', addBn);
     $('#obtainH').on('click', addH);
     function addU() {
         let btn = $('#obtainU')
         disableButton(btn)
+        
         let id = 0;
         if (address) {
             contract.initialize(id).then(() => {
@@ -58,55 +54,11 @@ $(function () {
 
 
     }
-    function addD() {
-        let btn = $('#obtainD')
-        disableButton(btn)
-        let id = 1;
-        if (address) {
-                contract.initialize(id).then(() => {
-                    contract.mint(address, 10000).then(res => {
-                        console.log(res)
-                        if (!res.success) {
-                            enableButton(btn)
-                            return;
-                        }
-                        enableButton(btn)
-                        alert('You  get 10000 DAI')
-                    }).catch(err => {
-                        enableButton(btn)
-                        console.log(err)
-                    })
-                });
-            
-        }
-
-    }
+    
     function addB() {
         let btn = $('#obtainB')
         disableButton(btn)
-        let id = 2;
-        if (address) {
-            contract.initialize(id).then(() => {
-                contract.mint(address, 10000).then(res => {
-                    console.log(res)
-                    if (!res.success) {
-                        enableButton(btn)
-                        return;
-                    }
-                    enableButton(btn)
-                    alert('You  get 10000 BAC')
-                }).catch(err => {
-                    enableButton(btn)
-                    console.log(err)
-                })
-            });
-        }
-
-    }
-    function addBn() {
-        let btn = $('#obtainBn')
-        disableButton(btn)
-        let id = 3;
+        let id = 1;
         if (address) {
             contract.initialize(id).then(() => {
                 contract.mint(address, 10000).then(res => {
@@ -125,10 +77,11 @@ $(function () {
         }
 
     }
+    
     function addH() {
         let btn = $('#obtainH')
         disableButton(btn)
-        let id = 4;
+        let id = 2;
         if (address) {
             contract.initialize(id).then(() => {
                 contract.mint(address, 10000).then(res => {
@@ -138,7 +91,7 @@ $(function () {
                         return;
                     }
                     enableButton(btn)
-                    alert('You  get 10000 HUSD')
+                    alert('You  get 10000 USDT')
                 }).catch(err => {
                     enableButton(btn)
                     console.log(err)
