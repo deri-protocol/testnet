e042938a965e9c2bd397ca8f5b6fbdb5822f6cbf
黎海冬 <haidong.li@dfactory.tech>
2021-05-26 17:59:01 +0800

    release

---
+++ release.sh
@@ -0,0 +1,10 @@
+cp index.html index.bak.html
+cp asset-manifest.json asset-manifest.bak.json
+cp manifest.json manifest.bak.json
+source ~/.bashrc
+yarn --nomap --cwd /Users/lihaidong/work/app.deri.finance build 
+cp -R /Users/lihaidong/work/app.deri.finance/build/* ./
+git checkout main
+git add .
+git commit -m 'release'
+git push origin main
