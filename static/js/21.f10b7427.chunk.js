5c4ed7149ac9bab6a4d8ffac257590ec075cd289
wenchao.huang <wenchao.huang@dfactory.tech>
2021-08-31 09:42:13 +0800

    release testnet

---
+++ static/js/21.f10b7427.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[21],{738:function(n,a,e){},739:function(n,a,e){},740:function(n,a,e){},773:function(n,a,e){"use strict";e.r(a);var c=e(647),t=(e(738),e(739),e(740),e(635)),i=(e(31),e(20));a.default=function({lang:n}){return Object(i.jsxs)("div",{className:"trade-container",children:[Object(i.jsx)(t.a,{lang:n}),Object(i.jsx)("div",{className:"trade-body",children:Object(i.jsx)(c.a,{lang:n})})]})}}}]);
+//# sourceMappingURL=21.f10b7427.chunk.js.map
---
+++ static/js/42.e96316b2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[42],{741:function(n,a,e){},774:function(n,a,e){"use strict";e.r(a),e.d(a,"default",(function(){return s}));var c=e(670),t=(e(741),e(632)),i=e(20);function s(n){var a=n.lang;return Object(i.jsxs)("div",{className:"home_box",children:[Object(i.jsx)(c.a,{lang:a}),Object(i.jsx)(t.a,{lang:a})]})}}}]);
+//# sourceMappingURL=42.e96316b2.chunk.js.map
